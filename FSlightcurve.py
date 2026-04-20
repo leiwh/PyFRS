19f235b7e586de2c4e45cb50205b37b07c7adc04
leiwh <leiwh78@gmail.com>
2026-04-20 10:17:54 +0800

    Create FSlightcurve.py

---
+++ FSlightcurve.py
@@ -0,0 +1,26 @@
+import numpy as np
+import matplotlib.pyplot as plt
+try:
+    from PyFRS import cgs,fs,rs
+except ImportError:
+    import cgs,fs,rs
+
+
+
+def FS_lc(v,**Z):
+
+    t=np.geomspace(1.e3,1.e8,500)
+    nu=np.empty(t.shape)
+    nu[:]=v
+
+    Fnu=fs.FS_flux(t,nu,**Z)
+
+    fig,ax=plt.subplots(1,1)
+    ax.plot(t,Fnu)
+    ax.set(xscale='log', xlabel=r'$t$ (s)',
+           yscale='log', ylabel=r'$F_\nu$[$10^{9}$ Hz] (mJy)')
+
+    fig.tight_layout()
+    print("Saving figure lc.png")
+    fig.savefig("FS_lc.png")
+    plt.close(fig)
